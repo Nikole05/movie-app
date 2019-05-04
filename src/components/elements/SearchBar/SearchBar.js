942edc846f40f1b377fe5f0efe70f7294d9fa201
Nico <nico@Nicos-MacBook-Pro.local>
2019-05-04 18:53:18 +0300

    add searchBar component

---
+++ src/components/elements/SearchBar/SearchBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import FontAwesome from 'react-fontawesome';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+    state = {
+
+    }
+
+    render(){
+        return (
+            <div>
+                SearchBar
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
