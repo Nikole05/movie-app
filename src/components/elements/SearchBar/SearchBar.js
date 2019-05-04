8f13667fcfffdc652bdd298934e574d1b7d335a6
Nico <nico@Nicos-MacBook-Pro.local>
2019-05-05 00:12:47 +0300

    Destructuring props & state for SearchBar comp

--- src/components/elements/SearchBar/SearchBar.js
+++ src/components/elements/SearchBar/SearchBar.js
@@ -7,19 +7,26 @@ class SearchBar extends Component {
     state = {
         value: ''
     }
-
+    // Must have this here so we can reset it
     timeout = null;
 
     doSearch = (event) => {
+        // ES6 Destructuring prop
+        const { callback } = this.props;
+
         this.setState({ value: event.target.value })
         clearTimeout(this.timeout);
-
+        // Set a timeout to wait for the user to stop writing
+        // So we don´t have to make unnessesary calls
         this.timeout = setTimeout(() => {
-            this.props.callback(this.state.value);
+            callback(this.state.value);
         }, 500);
     }
 
-    render(){
+    render() {
+        // ES6 Destructuring state
+        const { value } = this.state;
+
         return (
             <div className="rmdb-searchbar">
                 <div className="rmdb-searchbar-content">
@@ -29,12 +36,16 @@ class SearchBar extends Component {
                         className="rmdb-searchbar-input"
                         placeholder="Search"
                         onChange={this.doSearch}
-                        value={this.state.value}
+                        value={value}
                     />
                 </div>
             </div>
         )
     }
+}
+
+SearchBar.propTypes = {
+    callback: PropTypes.func
 }
 
 export default SearchBar;
