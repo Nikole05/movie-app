180a2807022d21154a94490da8a6cbaef20d0262
Nico <nico@Nicos-MacBook-Pro.local>
2019-05-04 19:59:45 +0300

    using fech to get API data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../config';
 import HeroImage from '../elements/HeroImage/HeroImage';
 import SearchBar from '../elements/SearchBar/SearchBar';
 import FourColGrid from '../elements/FourColGrid/FourColGrid';
@@ -8,21 +9,61 @@ import Spinner from '../elements/Spinner/Spinner';
 import './Home.css';
 
 class Home extends Component {
-state = {
+    state = {
+        movies: [],
+        heroImage: null,
+        loading: false,
+        currentPage: 0,
+        totalPages: 0,
+        searchTerm: ''
+    }
 
-}
+    componentDidMount() {
+            this.setState({ loading: true })
+            const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+            this.fetchItems(endpoint);
+        }
+    
 
-render() {
-    return (
-    <div className="rmdb-home">
-        <HeroImage />
-        <SearchBar />
-        <FourColGrid /> 
-        <Spinner />
-        <LoadMoreBtn />
-    </div>
-    )
-}
+    loadMoreItems = () => {
+
+        let endpoint = '';
+        this.setState({ loading: true })
+
+        if (searchTerm === '') {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${currentPage + 1}`;
+        } else {
+            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=${currentPage + 1}`;
+        }
+        this.fetchItems(endpoint);
+    }
+
+    fetchItems = (endpoint) => {
+
+        fetch(endpoint)
+            .then(result => result.json())
+            .then(result => {
+                this.setState({
+                    movies: [...movies, ...result.results],
+                    heroImage: heroImage || result.results[0],
+                    loading: false,
+                    currentPage: result.page,
+                    totalPages: result.total_pages
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div className="rmdb-home">
+                <HeroImage />
+                <SearchBar />
+                <FourColGrid />
+                <Spinner />
+                <LoadMoreBtn />
+            </div>
+        )
+    }
 }
 
 export default Home;
