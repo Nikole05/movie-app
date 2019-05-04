42db15e1dcbea7b8eac2461da3d70156fea19c7a
Nico <nico@Nicos-MacBook-Pro.local>
2019-05-04 17:45:37 +0300

    add home and header js

---
+++ src/components/elements/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="rmdb-header">
+            <div className="rmdb-header-content">
+                <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo" />
+                <img className="rmdb-tmdb-logo" src="./images/rmdb_logo.png" alt="tmbd-logo" />
+            </div>
+        </div>
+    )
+}
+export default Header;
