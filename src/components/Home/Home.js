a3f116e9379e04e49aa19688e50d4a7b75f2c6dd
Nico <nico@Nicos-MacBook-Pro.local>
2019-05-04 20:29:47 +0300

    HeroImage & SherachBar controled components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,6 +23,23 @@ class Home extends Component {
             const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
             this.fetchItems(endpoint);
         }
+
+    searchItems = (searchTerm) => {
+        console.log(searchTerm);
+        let endpoint = '';
+        this.setState({
+            movies: [],
+            loading: true,
+            searchTerm
+        })
+
+        if (searchTerm === "") {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        } else {
+            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+        }
+        this.fetchItems(endpoint);
+    }
     
 
     loadMoreItems = () => {
@@ -30,10 +47,10 @@ class Home extends Component {
         let endpoint = '';
         this.setState({ loading: true })
 
-        if (searchTerm === '') {
-            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${currentPage + 1}`;
+        if (this.state.searchTerm === '') {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
         } else {
-            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=${currentPage + 1}`;
+            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
         }
         this.fetchItems(endpoint);
     }
@@ -44,20 +61,28 @@ class Home extends Component {
             .then(result => result.json())
             .then(result => {
                 this.setState({
-                    movies: [...movies, ...result.results],
-                    heroImage: heroImage || result.results[0],
+                    movies: [...this.state.movies, ...result.results],
+                    heroImage: this.state.heroImage || result.results[0],
                     loading: false,
                     currentPage: result.page,
                     totalPages: result.total_pages
                 })
             })
+            .catch(error => console.error('Error:', error))
     }
 
     render() {
         return (
             <div className="rmdb-home">
-                <HeroImage />
-                <SearchBar />
+                {this.state.heroImage ?
+                    <div>
+                        <HeroImage
+                            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.heroImage.backdrop_path}`}
+                            title={this.state.heroImage.original_title}
+                            text={this.state.heroImage.overview}
+                        />
+                        <SearchBar callback={this.searchItems} />
+                    </div> : null}
                 <FourColGrid />
                 <Spinner />
                 <LoadMoreBtn />
