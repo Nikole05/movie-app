2f8565beb0d4001238d603ed5f3cb920349eecb0
Nico <nico@Nicos-MacBook-Pro.local>
2019-05-04 18:49:15 +0300

    add heroImage component

---
+++ src/components/elements/HeroImage/HeroImage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './HeroImage.css';
+
+const HeroImage = (props => {
+    return (
+        <div>
+            HeroImage
+        </div>
+    )
+})
+
+
+export default HeroImage;
