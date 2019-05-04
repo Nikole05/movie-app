e977e6068ac1faec79a1cf7df36e4c44c0ab18e1
Nico <nico@Nicos-MacBook-Pro.local>
2019-05-04 21:16:17 +0300

    finishing the components for home page

--- src/components/elements/FourColGrid/FourColGrid.js
+++ src/components/elements/FourColGrid/FourColGrid.js
@@ -3,9 +3,21 @@ import PropTypes from 'prop-types';
 import './FourColGrid.css';
 
 const FourColGrid = (props) => {
+    const renderElements = () => {
+        const gridElements = props.children.map((element, i) => (
+            <div key={i} className="rmdb-grid-element">
+                {element}
+            </div>
+        ))
+        return gridElements;
+    }
+
     return (
-        <div>
-            FourColGrid
+        <div className="rmdb-grid">
+            {props.header && !props.loading ? <h1>{props.header}</h1> : null}
+            <div className="rmdb-grid-content">
+                {renderElements()}
+            </div>
         </div>
     )
 }
--- src/components/elements/LoadMoreBtn/LoadMoreBtn.js
+++ src/components/elements/LoadMoreBtn/LoadMoreBtn.js
@@ -3,9 +3,10 @@ import './LoadMoreBtn.css';
 
 const LoadMoreBtn = (props) => {
     return (
-        <div>
-            Load More
-        </div>
+        
+            <div className="rmdb-loadmorebtn" onClick={props.onClick}>
+                <p>{props.text}</p>
+            </div>
     )
 }
 
