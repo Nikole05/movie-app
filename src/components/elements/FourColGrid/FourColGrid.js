8f0ed397c01aaf10b4369d8c685315a2da55f6ec
Nico <nico@Nicos-MacBook-Pro.local>
2019-05-04 19:00:51 +0300

    add fourColGrid & MovieThimb component

---
+++ src/components/elements/FourColGrid/FourColGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './FourColGrid.css';
+
+const FourColGrid = (props) => {
+    return (
+        <div>
+            FourColGrid
+        </div>
+    )
+}
+
+export default FourColGrid;
---
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './MovieThumb.css';
+
+const MovieThumb = (props) => {
+    return (
+        <div>
+            MovieThumb
+        </div>
+    )
+}
+
+export default MovieThumb;
